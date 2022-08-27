8e908eac5e67a8609d70023981da5ec64e6adbbc
E-t0m <110770475+E-t0m@users.noreply.github.com>
2022-08-27 13:36:08 +0200

    Update zeroinput.py
    improvement of low battery regulation

--- zeroinput.py
+++ zeroinput.py
@@ -5,7 +5,9 @@ serial_port		= '/dev/rs485'
 # data pipe from vzlogger, set as log in /etc/vzlogger.conf, "verbosity": 15 required, use mkfifo to create it before vz starts!
 vzlogger_log_file	= '/tmp/vz/vzlogger.fifo'
 persistent_vz_file	= '/var/log/vzlogger.log'
-number_of_gti		= 2
+number_of_gti		= 2 # units
+max_bat_discharge	= 1250 # W
+max_night_input		= 200 # W
 
 from sys import argv
 
@@ -23,7 +25,7 @@ from datetime import timedelta, datetime
 
 def handle_data(d):	# display the esmart data
 	if not verbose: return
-	battery_cur	= d['chg_cur'] - d['load_cur']
+	battery_cur		= d['chg_cur'] - d['load_cur']
 	battery_power	= d['chg_power'] - d['load_power']
 	system("clear")
 	print("%s\t SOC %i\t Mode %s"			% (strftime("%H:%M:%S"),		d['soc'],	esmart.DEVICE_MODE[d['chg_mode']]))
@@ -42,7 +44,7 @@ def set_soyo_demand(ser,power):	# create the packet for soyosource gti
 	ser.flush()
 	return(0)
 
-def close_values(a,b,tol): # check if values a and b are within tolerance
+def close_values(a,b,tol):	# check if values a and b are within tolerance
 	if a > b * (1 - 0.01*tol) and a < b *(1 + 0.01*tol): return(1)
 	return(0)
 
@@ -50,18 +52,20 @@ def avg(inlist):	# return the average of a list variable
 	if len(inlist) == 0: return(0)
 	return( sum(inlist) / len(inlist) )
 
+debug		= False
 send_power	= 0
 last_send	= 0
 last_runtime 	= 0
 bat_cont	= 0
 pv_cont		= 0
-bat_history	= [0]*10	# history vars with *n interval steps
+bat_history	= [0]*20	# history vars with *n interval steps
 pv_history	= [0]*20
 extra_history	= [0]*8
 send_history	= [0]*4
-pv_red		= 0.9	# PV reduction on low battery in % / 100
-powercurve	= [0,0,1,3,6,9,12,15,18,22,26,30,34,39,45,51,60,70,80,90,100]	# in %
 
+bat_power_minus	= -25	# W static reduction on low battery
+pv_red_factor	= 0.86	# PV reduction on low battery in % / 100
+powercurve	= [0,2,3,4,5,6,7,13,17,21,26,30,34,39,45,52,60,70,80,90,100] # in %
 
 timeout_repeat	= datetime.now()
 vz_in		= open(vzlogger_log_file,'r')
@@ -78,7 +82,6 @@ while True:	# infinite loop, stop the script with ctl+c
 		esm.close()
 		d = esm.export()	# get esmart values
 		
-
 		main_log = False; Ls_read = 99999; Ls_ts = 99999
 		last2_send = last_send; last_send = send_power
 		
@@ -88,7 +91,7 @@ while True:	# infinite loop, stop the script with ctl+c
 			if '[main] vzlogger' in l: 
 				main_log = True
 				vzout = open(persistent_vz_file,'a')
-				vzout.write('REDIRECTED by zeroinput.py from /home/vzlogger/vzlogger.fifo\n')
+				vzout.write('REDIRECTED by zeroinput.py from'+ vzlogger_log_file +'\n')
 				if verbose: print('\nvzlogger restart')
 			elif 'Startup done.' in l:
 				main_log = False
@@ -114,11 +117,21 @@ while True:	# infinite loop, stop the script with ctl+c
 		
 		status_text = ''
 		bat_history = bat_history[1:] + [ d['bat_volt']]
+		
 		if 0 in bat_history: bat_cont = d['bat_volt']
-		else: bat_cont = avg(bat_history)
+		else: 
+			sort_bat = bat_history[:]
+			sort_bat.sort()
+			bat_cont = avg(sort_bat[:10]) # average on low pass
+			if debug: print('\nsort_bat\t',sort_bat,'\nlow pass\t',sort_bat[0:10])
+		
+		if debug: print('bat_history\t',bat_history,'\nbat_cont\t',bat_cont)
 		
 		pv_history = pv_history[1:]+ [d['chg_power']]
-		pv_cont = int(avg(pv_history))
+		sort_pv = pv_history[:]
+		sort_pv.sort()
+		pv_cont = int(avg(sort_pv[-10:])) # average on high pass, remove gap on mppt tracker restart
+		if debug: print('pv_history\t', pv_history,'\nsort_pv\t\t',sort_pv,'\npv_cont\t\t',pv_cont)
 		
 		if datetime.now() < timeout_repeat:	# battery protection timeout
 			send_power = 0
@@ -133,20 +146,20 @@ while True:	# infinite loop, stop the script with ctl+c
 			
 			elif bat_cont >= 48 and bat_cont <= 50:	# limit to pv power, by battery voltage
 				if send_power > pv_cont:
-					bat_p_index = int(round(bat_cont*10-480,0))
-					bat_power = int(powercurve[ bat_p_index ] * 12)	# maximum 1200 W at 50 V
+					bat_p_index = int(bat_cont*10-480)
+					bat_power = int(powercurve[ bat_p_index ] * max_bat_discharge/100)	# maximum at 50 V
 					extra_history = extra_history[1:]+[bat_power]
 					if 0 in extra_history: pass	# bat_power remains at last calculated value
 					else: bat_power = avg(extra_history)
 					
-					if verbose: status_text	= ' PV %i W (%i%%), bat %i W (%i%%)' % (int(pv_cont*pv_red), int(pv_red*100), bat_power, powercurve[bat_p_index])
+					if verbose: status_text	= ' PV %i W (%i%%), bat %i W (%i%%), static %i W' % (int(pv_cont*pv_red_factor), int(pv_red_factor*100), bat_power, powercurve[bat_p_index],bat_power_minus)
 					
-					if	send_power > int( pv_cont*pv_red + bat_power):
-						send_power = int( pv_cont*pv_red + bat_power)
+					if	send_power > int( pv_cont*pv_red_factor + bat_power + bat_power_minus):
+						send_power = int( pv_cont*pv_red_factor + bat_power + bat_power_minus)
 						if verbose: status_text =' limited ' + status_text
 			
-			if d['chg_power'] == 0 and send_power > 200: # night limit
-				send_power = 200
+			if pv_cont == 0 and send_power > max_night_input: # night limit
+				send_power = max_night_input
 				if verbose: status_text = 'night limit'
 			
 			if bat_cont > 55.5:	# give some free power to the world if bat > 55.5 V
@@ -155,8 +168,8 @@ while True:	# infinite loop, stop the script with ctl+c
 				if verbose: status_text = 'over export '+str(free_power)+' W'
 			else: free_power = 0
 			
-			if send_power > 1200 + d['chg_power']:	# battery discharge limit 1200 W
-				send_power = int(1200 + d['chg_power'])
+			if send_power > max_bat_discharge + d['chg_power']:	# battery discharge limit
+				send_power = int(max_bat_discharge + d['chg_power'])
 				if verbose: status_text	= 'battery current limit'
 			
 			send_history = send_history[1:]+[send_power]	# build a send_power history
@@ -177,14 +190,14 @@ while True:	# infinite loop, stop the script with ctl+c
 				status_text = 'MIN power limit'
 				
 			if send_power	> 900 * number_of_gti:	# the limit of the gti
-				send_power = 900 * number_of_gti
+				send_power	= 900 * number_of_gti
 				status_text = 'MAX power limit'
 			
 		with open('/tmp/vz/soyo.log','w') as fo:	# send some values to vzlogger
-			fo.write('%i: soyosend = %i\n'		% ( time(), -1*send_power ) )	# the keywords have to be created 
-			fo.write('%i: pv_w = %i\n'		% ( time(), -1*d['chg_power'] ) )	# as channels in vzlogger to make it work there!
-			fo.write('%i: pv_u = %f\n'		% ( time(), d['pv_volt']  ) )
-			fo.write('%i: bat_v = %f\n'		% ( time(), d['bat_volt'] ) )
+			fo.write('%i: soyosend = %i\n'	% ( time(), -1*send_power ) )	# the keywords have to be created 
+			fo.write('%i: pv_w = %i\n'	% ( time(), -1*d['chg_power'] ) )	# as channels in vzlogger to make it work there!
+			fo.write('%i: pv_u = %f\n'	% ( time(), d['pv_volt']  ) )
+			fo.write('%i: bat_v = %f\n'	% ( time(), d['bat_volt'] ) )
 			fo.write('%i: int_temp = %i\n'	% ( time(), d['int_temp'] ) )
 			fo.write('%i: bat_temp = %i\n'	% ( time(), d['bat_temp'] ) )
 			fo.write('%i: panel_w = %i\n'	% ( time(), -1*free_power ) )
