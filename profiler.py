b925e0f2593938afcd96e7ea0c83b6b253da32f5
E-t0m <110770475+E-t0m@users.noreply.github.com>
2024-11-14 16:58:31 +0100

    more precise vz data

--- profiler.py
+++ profiler.py
@@ -5,13 +5,12 @@ from requests import get
 from datetime import timedelta, datetime
 from sys import argv
 
-chans = {		'Erzeug':'your-UUID-here',
-				'Bezug':'your-UUID-here',
-				'tibber':'your-UUID-here',
-				'Auto':'your-UUID-here',
-				'Klima':'your-UUID-here',
-				'PV':'your-UUID-here',
-				
+chans = {		'Wh Erzeug':	'your-vz-UUID-here',
+				'Wh Bezug':		'your-vz-UUID-here',
+				'¢/kWh tibber':	'your-vz-UUID-here',
+				'Wh Auto':		'your-vz-UUID-here',
+				'Wh Klima':		'your-vz-UUID-here',
+				'Wh PV':		'your-vz-UUID-here',
 		}
 
 try:
@@ -26,22 +25,22 @@ except:
 	day_shift = 0
 
 def get_average(n_days,day_shift):
-	hours = {}
+	hours = dict()
 	for key in chans.keys(): hours[key] = [0.0]*24
 	
 	for day in range(0,n_days):
 		begin	= (datetime.today() - timedelta(days=day+day_shift,hours=24)).replace(minute=0,second=0,microsecond=0)
-		end		= (datetime.today() - timedelta(days=day+day_shift,hours=1 )).replace(minute=50)
+		end		= (datetime.today() - timedelta(days=day+day_shift,hours=0 )).replace(minute=0,second=0,microsecond=0)
 		beginstamp	= str(int(begin.timestamp())).ljust(13,'0')
 		endstamp	= str(int(end.timestamp())).ljust(13,'0')
 		url = 'http://127.0.0.0:8080/data.json?from='+beginstamp+'&to='+endstamp+'&group=hour'	# asumes volkszähler at localhost:8080
 		for uuid in chans.values(): url += '&uuid[]='+uuid
 	
 		if True: 
-			print('\n',day, '\tbegin\t',begin,beginstamp,'\n',day,'\tend\t',end,endstamp )
+			print('\n',day, '\tbegin',begin,beginstamp,'\tend',end,endstamp,'\t',end='')
 			begin_request = datetime.now()
 		jresp = get(url=url).json()
-		print('request duration:', datetime.now()-begin_request)
+		print('request duration:', datetime.now()-begin_request,'\trows:',jresp['data'][0]['rows'])
 		
 		#print('chan\t\tday\thour\tval')
 		for row in jresp['data']:
@@ -51,11 +50,12 @@ def get_average(n_days,day_shift):
 			
 			for value in row['tuples']:
 				tval = datetime.fromtimestamp(value[0]/1000)
-				#print('%s\t\t%i\t%i\t% .2f'%(chan_n,tval.day,tval.hour,value[1])) # show hourly values
+				if tval > end: continue												# drop next day values sometimes send from vz
+				#print('%s\t\t%i\t%i\t% .2f'%(chan_n,tval.day,tval.hour,value[1]))	# show hourly values by channel
 				hours[chan_n][tval.hour] += value[1]
 		
 		if True:
-			print("\nday\thour\t%s"%(list(chans)))		# show summary
+			print("\nday\thour\t%s"%(list(chans)))		# show daily summary
 			for i in range(0,24): 
 				hourline = ''
 				for key in chans: hourline += '% 8.2f\t'%hours[key][i]
@@ -66,18 +66,14 @@ def get_average(n_days,day_shift):
 			hours[key][i] /= n_days
 	
 	if True:											# calculate new "channels"
-		hours['Kosten-Bezug'] = [0.0]*24
-		hours['Wert-Erzeug'] = [0.0]*24
-		#hours['Bezug+Erzeug'] = [0.0]*24
-		#hours['Auto+Klima'] = [0.0]*24
-		#hours['Bezug+Erzeug-Auto-Klima'] = [0.0]*24
+		hours['¢/h Kosten Bezug-Auto'] = [0.0]*24
+		hours['¢/h Wert-Erzeug'] = [0.0]*24
+		hours['Wh Bezug-Auto'] = [0.0]*24
 		
 		for i in range(0,24):
-			hours['Kosten-Bezug'][i] = (hours['Bezug'][i] * hours['tibber'][i]) / 1000			# Wh * ¢/kWh / 1000 = ¢/h
-			hours['Wert-Erzeug'][i] = abs(hours['Erzeug'][i]) * hours['tibber'][i] / 1000		# Wh * ¢/kWh / 1000 = ¢/h
-			#hours['Bezug+Erzeug'][i] = hours['Bezug'][i] + abs(hours['Erzeug'][i])				# total consumption [Wh]
-			#hours['Auto+Klima'][i] = hours['Auto'][i] + hours['Klima'][i]
-			#hours['Bezug+Erzeug-Auto-Klima'][i] = hours['Bezug'][i] + abs(hours['Erzeug'][i]) - hours['Auto'][i] - hours['Klima'][i]
+			hours['Wh Bezug-Auto'][i] = hours['Wh Bezug'][i] - hours['Wh Auto'][i]
+			hours['¢/h Kosten Bezug-Auto'][i] = (hours['Wh Bezug-Auto'][i] * hours['¢/kWh tibber'][i]) / 1000	# Wh * ¢/kWh / 1000 = ¢/h
+			hours['¢/h Wert-Erzeug'][i] = abs(hours['Wh Erzeug'][i]) * hours['¢/kWh tibber'][i] / 1000			# Wh * ¢/kWh / 1000 = ¢/h
 		
 	if True:											# show average values
 		headerline = ''
@@ -90,6 +86,13 @@ def get_average(n_days,day_shift):
 			for key in hours.keys(): 
 				hourline += '%.0f\t'%hours[key][i]
 			print('%i\t%s'%(i,hourline))
+		
+		if True:		# summary 
+			hourline = ''
+			for key in hours.keys(): 
+				hourline += '%.0f\t'%sum(hours[key])
+			print('sum\t%s'%hourline)
+			#print('\n',day, '\tbegin',begin,beginstamp,'\tend',end,endstamp)
 	
 	return(hours)
 
@@ -98,4 +101,6 @@ print(n_days,'days to go', 'shift '+str(day_shift) if day_shift else '')
 avg_day = get_average(n_days,day_shift)
 
 exit(0)
+
+
 
