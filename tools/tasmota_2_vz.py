0e38b8d2a6374afbe80de10e00a9ab400832cdd5
E-t0m <110770475+E-t0m@users.noreply.github.com>
2026-05-29 10:53:34 +0200

    Update and rename tasmota_2_vz.py to tools/tasmota_2_vz.py

--- tools/tasmota_2_vz.py
+++ tools/tasmota_2_vz.py
@@ -5,8 +5,9 @@ import requests
 from time import time, sleep
 
 debug = False
+sleep(7) # shift network traffic
 
-url = 'http://111.222.333.444/cm?cmnd=status%2010'
+url = 'http://192.168.178.104/cm?cmnd=status%2010'
 z = 0
 for i in range(0,3):
 	resp = requests.get(url=url)
@@ -20,11 +21,12 @@ for i in range(0,3):
 		sleep(20)
 		pass
 	else:
+		power = sum(power)	# 2 way power sensor in parallel
 		for j in range(0,20):
-			with open('/tmp/vz/soyo.log','a') as fo:
-				fo.write('%i: klima_p = %i\n'	% ( time(), power ) )
+			with open('/tmp/vz/output_to_vz.log','a') as fo:
+				fo.write('%i: pool_p = %i\n'	% ( time(), power ) )
 			if debug:
-				print('%i: klima_p = %i\n'	% ( time(), power ) )
+				print('%i: pool_p = %i\n'	% ( time(), power ) )
 				z += 1
 				print(i,j,z)
 			sleep(1)
